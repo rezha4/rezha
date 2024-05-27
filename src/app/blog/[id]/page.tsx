e9820b037a7579f627d2051202bae026cf892950
rezha4 <rezha.bahari@gmail.com>
2024-05-27 13:58:56 +0700

    BlogPost i/o BlogPage for [id]

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -2,8 +2,8 @@ export function generateStaticParams() {
   return [{ id: '1' }, { id: '2' }, { id: '3' }]
 }
 
-const BlogPage = ({ params }: { params: { id: string } }) => {
+const BlogPost = ({ params }: { params: { id: string } }) => {
   return <div>youre at {params.id}</div>;
 };
 
-export default BlogPage;
+export default BlogPost;
