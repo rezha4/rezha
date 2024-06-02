63bf6b6fd238f4a7979a2fc2b862afe64355bc29
rezha4 <rezha.bahari@gmail.com>
2024-06-02 18:27:11 +0700

    dot slash for importing

--- src/components/shared/profile-photo.tsx
+++ src/components/shared/profile-photo.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const ProfilePhoto = () => {
   return (
       <img
-        src="/rezha-bahari-hero.webp"
+        src="./rezha-bahari-hero.webp"
         alt="Profile Photo"
         className="w-40 h-40 sm:w-64 sm:h-64 rounded-full shadow-xl"
       />
